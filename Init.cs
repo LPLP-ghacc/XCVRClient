ae32414525519956be678fb17d4b0d6618127b8a
LPLP <lilpophill@gmail.com>
2023-01-14 08:02:26 +0200

    first commit

---
+++ Init.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XCVRClient;
+using MelonLoader;
+
+namespace XCVRClient
+{
+    public class Init : MelonMod
+    {
+        [Obsolete]
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("pip?");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using XCVRClient;
+
+[assembly: AssemblyTitle("XCVRClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XCVRClient")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6f960134-6b91-4f09-872e-8b30fe469fc6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(Init), "XCVRClient", "1.0.0", "LPLP")]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
